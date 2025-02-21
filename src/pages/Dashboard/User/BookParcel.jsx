194da78ed6bcd239abb72293775a080a0b8d0010
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-02-21 12:20:06 +0600

    Book Parcel Form Added

--- src/pages/Dashboard/User/BookParcel.jsx
+++ src/pages/Dashboard/User/BookParcel.jsx
@@ -1,10 +1,210 @@
-
+import { useContext, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import AuthContext from "../../../context/AuthContext/AuthContext";
 const BookParcel = () => {
-    return (
-        <div>
-            hi
-        </div>
-    );
+  const { user } = useContext(AuthContext);
+  const { register, handleSubmit, setValue, watch } = useForm();
+  const parcelWeight = watch("weight");
+  useEffect(() => {
+    if (user) {
+      setValue("name", user?.displayName || "");
+      setValue("email", user?.email || "");
+    }
+  }, [user, setValue]);
+  const onSubmit = (data) => {
+    console.log("Submitted Data:", data);
+    alert(JSON.stringify(data, null, 2));
+  };
+  useEffect(() => {
+    if (parcelWeight) {
+      const weight = parseFloat(parcelWeight);
+      let charge = 50;
+      if (weight == 2) {
+        charge = 100;
+      } else if (weight > 2) {
+        charge = 150;
+      }
+      setValue("deliveryCharge", charge.toFixed(2));
+    }
+  }, [parcelWeight, setValue]);
+
+  return (
+    <div>
+      <div>
+        <h1 className="text-2xl font-bold text-center my-4">BOOK A PARCEL</h1>
+      </div>
+      {/* FORM */}
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+            {/* NAME */}
+            <label className="form-control w-full  flex flex-col">
+              <div className="label">
+                <span className="label-text">Name*</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Name"
+                readOnly
+                {...register("name")}
+                className="input input-bordered w-full "
+              />
+            </label>
+
+            {/* EMAIL */}
+            <label className="form-control w-full  flex flex-col">
+              <div className="label">
+                <span className="label-text">Email*</span>
+              </div>
+              <input
+                type="email"
+                readOnly
+                placeholder="Email"
+                {...register("email")}
+                className="input input-bordered w-full "
+              />
+            </label>
+
+            {/* PHONE */}
+            <label className="form-control w-full  flex flex-col">
+              <div className="label">
+                <span className="label-text">Phone Number*</span>
+              </div>
+              <input
+                type="number"
+                placeholder="Phone"
+                {...register("phone")}
+                className="input input-bordered w-full "
+              />
+            </label>
+
+            {/* WEIGHT */}
+            <label className="form-control w-full  flex flex-col">
+              <div className="label">
+                <span className="label-text">Parcel Weight (kg)*</span>
+              </div>
+              <input
+                type="number"
+                placeholder="Weight"
+                {...register("weight")}
+                className="input input-bordered w-full "
+              />
+            </label>
+
+            {/* TYPE */}
+            <label className="form-control w-full  flex flex-col">
+              <div className="label">
+                <span className="label-text">Parcel Type*</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Type"
+                {...register("type")}
+                className="input input-bordered w-full "
+              />
+            </label>
+
+            {/* RECEIVERS */}
+            <label className="form-control w-full  flex flex-col">
+              <div className="label">
+                <span className="label-text">Receivers Name*</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Name"
+                {...register("receiversName")}
+                className="input input-bordered w-full "
+              />
+            </label>
+
+            {/* RECEIVERS PHONE */}
+            <label className="form-control w-full  flex flex-col">
+              <div className="label">
+                <span className="label-text">Receivers Phone*</span>
+              </div>
+              <input
+                type="number"
+                placeholder="Phone"
+                {...register("receiversPhone")}
+                className="input input-bordered w-full "
+              />
+            </label>
+
+            {/* DELIVERY ADDRESS */}
+            <label className="form-control w-full  flex flex-col">
+              <div className="label">
+                <span className="label-text">Delivery Address*</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Address"
+                {...register("deliveryAddress")}
+                className="input input-bordered w-full "
+              />
+            </label>
+
+            {/* DELIVERY DATE */}
+            <label className="form-control w-full  flex flex-col">
+              <div className="label">
+                <span className="label-text"> Delivery Date*</span>
+              </div>
+              <input
+                type="date"
+                placeholder="Date"
+                {...register("deliveryDate")}
+                className="input input-bordered w-full "
+              />
+            </label>
+
+            {/* DELIVERY LATITUDE */}
+            <label className="form-control w-full  flex flex-col">
+              <div className="label">
+                <span className="label-text"> Delivery Address Latitude*</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Latitude"
+                {...register("latitude")}
+                className="input input-bordered w-full "
+              />
+            </label>
+
+            {/* DELIVERY LONGITUDE */}
+            <label className="form-control w-full  flex flex-col">
+              <div className="label">
+                <span className="label-text"> Delivery Address Longitude*</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Longitude"
+                {...register("longitude")}
+                className="input input-bordered w-full "
+              />
+            </label>
+
+            {/* DELIVERY CHARGE */}
+            <label className="form-control w-full  flex flex-col">
+              <div className="label">
+                <span className="label-text">Delivery Charge*</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Charge"
+                {...register("deliveryCharge")}
+                readOnly
+                className="input input-bordered w-full "
+              />
+            </label>
+          </div>
+
+          {/* SUBMIT BTN */}
+          <div className="flex justify-center my-3">
+            <input className="btn btn-primary" type="submit" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default BookParcel;
